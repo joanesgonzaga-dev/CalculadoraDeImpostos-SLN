af1ec54686f08aa862482b8c30ba3c0d2cd1453f
Joanes Gonzaga <joanesgonzaga.dev@gmail.com>
2023-06-30 10:52:05 -0300

    New

---
+++ CalculadoraDeImpostos/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDeImpostos
+{
+    public class Item
+    {
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+
+        public Item(string nome, double valor)
+        {
+            Nome = nome;
+            Valor = valor;
+        }
+    }
+}
--- CalculadoraDeImpostos/Orcamento.cs
+++ CalculadoraDeImpostos/Orcamento.cs
@@ -9,10 +9,16 @@ namespace CalculadoraDeImpostos
     public class Orcamento
     {
         public double Valor { get; private set; }
-
+        public List<Item> Itens { get; set; }
         public Orcamento(double valor)
         {
             Valor = valor;
+            Itens = new List<Item>();
+        }
+
+        public void AdicionaItem(Item item)
+        {
+            Itens.Add(item);
         }
     }
 }
