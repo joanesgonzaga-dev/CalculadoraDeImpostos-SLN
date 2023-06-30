cdd0bb590b8ef61736c1cd925217d315d00623cd
Taynah Braz <Taynah Braz@LAPTOP-4B744Q2I>
2023-06-29 21:01:23 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraDeImpostos/CalculadoraDeImposto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDeImpostos
+{
+    public class CalculadoraDeImposto
+    {
+        public void RealizaCalculo(Orcamento orcamento, Imposto imposto)
+        {
+            double valor = imposto.Calcula(orcamento);
+
+            Console.WriteLine(valor);
+        }
+    }
+}
---
+++ CalculadoraDeImpostos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraDeImpostos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orcamento orcamento = new Orcamento(2500.00);
+
+            Imposto icms = new ICMS();
+            Imposto iss = new ISS();
+
+            CalculadoraDeImposto calculadora = new CalculadoraDeImposto();
+
+            calculadora.RealizaCalculo(orcamento, icms);
+            calculadora.RealizaCalculo(orcamento, iss);
+
+            Console.Read();
+        }
+    }
+}
